3a44a89ce878fcfec93c68ba015e8179838c952c
Natanael Amaral de Barros <natanael.barros@fabricads.onmicrosoft.com>
2025-02-03 18:49:57 -0300

    base da api

---
+++ teste_tokio_api/src/main/java/com/example/teste_tokio_25/TesteTokio25Application.java
@@ -0,0 +1,13 @@
+package com.example.teste_tokio_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTokio25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTokio25Application.class, args);
+	}
+
+}
